e2f509640ad06bddc32c434fdac7dabdfa59784f
Mark <vernidub66@gmail.com>
2024-06-14 13:53:07 +0300

    Updated

--- jmix-petclinic-main/src/main/java/io/jmix/petclinic/security/AnonymousRestRole.java
+++ jmix-petclinic-main/src/main/java/io/jmix/petclinic/security/AnonymousRestRole.java
@@ -1,8 +1,10 @@
 package  io.jmix.petclinic.security;
+import io.jmix.petclinic.entity.User;
 import io.jmix.petclinic.entity.owner.Owner;
 import io.jmix.petclinic.entity.pet.Pet;
 import io.jmix.petclinic.entity.pet.PetType;
 import io.jmix.petclinic.entity.veterinarian.Specialty;
+import io.jmix.petclinic.entity.visit.Visit;
 import io.jmix.petclinic.entity.visit.VisitType;
 import io.jmix.security.model.EntityAttributePolicyAction;
 import io.jmix.security.model.EntityPolicyAction;
@@ -18,11 +20,15 @@ public interface AnonymousRestRole {
     @EntityAttributePolicy(entityClass = Pet.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityAttributePolicy(entityClass = PetType.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityAttributePolicy(entityClass = Specialty.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
-    // @EntityAttributePolicy(entityClass = Owner.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
-    // @EntityAttributePolicy(entityClass = VisitType.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityAttributePolicy(entityClass = User.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    @EntityAttributePolicy(entityClass = Owner.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityAttributePolicy(entityClass = Visit.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityPolicy(entityClass = Pet.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = PetType.class, actions = EntityPolicyAction.ALL)
-    @EntityPolicy(entityClass = Specialty   .class, actions = EntityPolicyAction.ALL)
+    @EntityPolicy(entityClass = Specialty.class, actions = EntityPolicyAction.ALL)
+    @EntityPolicy(entityClass = User.class, actions = EntityPolicyAction.ALL)
+    @EntityPolicy(entityClass = Owner.class, actions = EntityPolicyAction.ALL)
+    @EntityPolicy(entityClass = Visit.class, actions = EntityPolicyAction.ALL)
 
     void country();
 }
