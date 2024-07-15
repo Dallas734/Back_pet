4822e4ba7932253efaad74526ac8efe7d669652e
Mark <vernidub66@gmail.com>
2024-07-15 10:24:13 +0300

    New features

--- jmix-petclinic-main/src/main/java/io/jmix/petclinic/security/AnonymousRestRole.java
+++ jmix-petclinic-main/src/main/java/io/jmix/petclinic/security/AnonymousRestRole.java
@@ -4,6 +4,7 @@ import io.jmix.petclinic.entity.owner.Owner;
 import io.jmix.petclinic.entity.pet.Pet;
 import io.jmix.petclinic.entity.pet.PetType;
 import io.jmix.petclinic.entity.veterinarian.Specialty;
+import io.jmix.petclinic.entity.veterinarian.Veterinarian;
 import io.jmix.petclinic.entity.visit.Visit;
 import io.jmix.petclinic.entity.visit.VisitType;
 import io.jmix.security.model.EntityAttributePolicyAction;
@@ -23,12 +24,16 @@ public interface AnonymousRestRole {
     @EntityAttributePolicy(entityClass = User.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
     @EntityAttributePolicy(entityClass = Owner.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityAttributePolicy(entityClass = Visit.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityAttributePolicy(entityClass = Veterinarian.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityAttributePolicy(entityClass = Specialty.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityPolicy(entityClass = Pet.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = PetType.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = Specialty.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = User.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = Owner.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = Visit.class, actions = EntityPolicyAction.ALL)
+    @EntityPolicy(entityClass = Veterinarian.class, actions = EntityPolicyAction.ALL)
+    @EntityPolicy(entityClass = Specialty.class, actions = EntityPolicyAction.ALL)
 
     void country();
 }
